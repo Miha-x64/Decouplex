0c6f27b9694c5d12efe9e4e91a394e5aeecc5fe0
Miha_x64 <mihlo1996@rambler.ru>
2016-08-22 13:04:03 +0300

    Annotations target

--- decouplex/src/main/java/net/aquadc/decouplex/annotation/OnError.java
+++ decouplex/src/main/java/net/aquadc/decouplex/annotation/OnError.java
@@ -1,12 +1,15 @@
 package net.aquadc.decouplex.annotation;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Created by miha on 14.05.16.3
  * for docs {@see OnResult}
  */
+@Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface OnError {
     Class face() default Void.class;
--- decouplex/src/main/java/net/aquadc/decouplex/annotation/OnResult.java
+++ decouplex/src/main/java/net/aquadc/decouplex/annotation/OnResult.java
@@ -1,12 +1,15 @@
 package net.aquadc.decouplex.annotation;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Created by miha on 14.05.16.3
  *
  */
+@Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface OnResult {
     Class face() default Void.class; // handling results on _face_ interface; Void means any
